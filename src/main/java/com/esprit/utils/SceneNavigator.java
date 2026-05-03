c3a5f92e14d883eb9d15f29ac2dfa0e97d961552
mariem <mariemabbes098@gmail.com>
2026-05-03 23:35:45 +0100

    Fix LoadException on EventAdmin.fxml due to invalid stylesheets syntax

--- src/main/java/com/esprit/utils/SceneNavigator.java
+++ src/main/java/com/esprit/utils/SceneNavigator.java
@@ -56,6 +56,7 @@ public final class SceneNavigator {
 
             fadeOut.play();
         } catch (IOException e) {
+            e.printStackTrace();
             if (errorHandler != null) {
                 errorHandler.accept("Unable to open page.");
             }
